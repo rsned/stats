197d996791cd6293f1d6a4ef0b8f141c691c69a8
Robert Snedegar <viking@gmail.com>
2025-07-10 13:40:49 -0700

    add Github Actions

---
+++ doc.go
@@ -0,0 +1,9 @@
+/*
+Package stats is the top level package for a variety of stats packages that provide
+routines for performing statistical calculations.
+
+Current packages include:
+
+	correlation/ - Methods for performing statistical correlation on datasets.
+*/
+package stats
